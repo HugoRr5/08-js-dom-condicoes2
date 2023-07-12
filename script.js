ea2cad8aa5e76b5c80018fb660dd8ca6d532d004
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-07-12 17:57:30 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 
 
-function carregar() {
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
-  
-    msg.innerHTML = `Agora são ${hora} horas.`;
-    if (hora >= 0 && hora < 12) {
-      // Bom Dia!!!
-      img.src = 'foto-da-manha.png';
-      document.body.style.background = '#e2cd9f';
-    } else if (hora >= 12 && hora < 18) {
-      // Boa Tarde!!!
-      img.src = 'foto-da-tarde.png';
-      document.body.style.background = '7eacc4';
-    } else {
-      // Boa Noite!!!
-      img.src = 'foto-da-noite.png';
-      document.body.style.background = '#00009C';
-    }
-  }
+
   
   function verificar() {
     var data = new Date();
